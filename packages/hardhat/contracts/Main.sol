59b8e7dc3876cc2ab766a36fc84df54dea651873
sonika-25 <64954778+sonika-25@users.noreply.github.com>
2021-08-04 17:41:29 +0530

    main

--- packages/hardhat/contracts/Main.sol
+++ packages/hardhat/contracts/Main.sol
@@ -31,3 +31,42 @@ contract Validator {
       }
 
 }
+
+/*
+contract DAO {
+    mapping(string => string[]) fields;
+
+    constructor() {
+        fields["Computer Science"] = [
+            "Theory of computation",
+            "Information and coding theory",
+            "Data structures and algorithms",
+            "Programming language theory and formal methods",
+            "Artificial intelligence",
+            "Game Theory",
+            "Computer architecture and organization",
+            "Concurrent, parallel and distributed computing",
+            "Computer networks",
+            "Computer security and cryptography",
+            "Databases and data mining",
+            "Computer graphics and visualization",
+            "Image and sound processing",
+            "Computational science, finance and engineering",
+            "Social computing and human computer interaction",
+            "Software engineering"
+        ];
+    }
+}
+
+contract Scholar {
+    address id;
+    string name;
+
+    // TODO: fields should be of type DAO.fields
+    // (deploy DAO contract, then fetch its fields here using address of DAO)
+    // or find some smart way to do it
+    mapping(string => string[]) fields;
+
+    mapping(string => bool) verification;
+}
+*/
